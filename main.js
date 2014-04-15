093e2d9c15e9c3cef98c3ea9df83343ae48dd8f1
W. Ryan Nestor <nestor@fuzzy-math.com>
2014-04-15 14:20:15 -0400

    comments

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ var data;
 var cScale;
 var val_keys;
 val_arr = [];
-	
-	
+var dm;	
+var ex;
+
+	//Load the two files, asynchronously.
 	$(function(){
 		queue()
 			.defer(d3.json, "country.json")
@@ -15,40 +17,45 @@ val_arr = [];
 	
 	
 var build = function(e, m , d){
-	map = m;
-	data = d;
-	var w = 1024,h=768;
+	map = m; //the map file as json obj
+	data = d; //the data as json obj
 	
-	val_keys = Object.keys(data.values);
+	var w = 1024,	h=768;
+	
+	val_keys = Object.keys(data.values); //need an array of keys, since the data.values is an object.
 
+	//loop through the values to create an array of them.
 	for (var i = 0; i<val_keys.length; i++){
 		var val = data.values[val_keys[i]];
-		val_arr.push(val);
-		
+		val_arr.push(val);	
 	}
 	
-	var ex = d3.extent(val_arr, function(d){return +d});
-	var step = Math.ceil(ex[1]-ex[0])/5;
-	var dm = d3.range(ex[0], ex[1], step)
+	//determine the extents of the values, for scaling.
+	ex = d3.extent(val_arr, function(d){return +d});
+	var step = Math.ceil(ex[1]-ex[0])/5;   //a "step" that is roughly one fifth of the array
+	dm = d3.range(ex[0], ex[1], step);   //a domain that contains the original extent, plus 3 "steps" to make 5 ranges
 	
-	cScale = d3.scale.linear().range(["blue","red","yellow","pink","red"])
+	cScale = d3.scale.linear().range(["blue","green","yellow","pink","red"])  //for these five color
 		.domain(dm);
 		
 	
-	var projection = d3.geo.mercator().scale(128).translate([w/2,h/2]);
+	var projection = d3.geo.mercator().scale(128).translate([w/2,h/2]);  //select projection for drawing GIS shapes
 
-	var pathG = d3.geo.path().projection(projection);
+	var pathG = d3.geo.path().projection(projection); //A path function for drawing shapes using the above projection
 
+	//select the body and append an svg
 	var svg = d3.select("body").append("svg")
 		.attr("width", w)
 		.attr("height", h);
 	
-	
+	//and a group for our shapes
 	var group = svg.append("g").attr("class","countries")
-		
+	
+	//then draw the shapes	
 	group.selectAll("path").data(map.features).enter().append("path")
 		.style({
 			"fill": function(d){  
+				//apply a scaled fill color, or grey if value is null, NaN
 				var val = +data.values[+d.properties.gid];
 				if(!isNaN(val)){
 					return cScale(val) 
@@ -59,8 +66,10 @@ var build = function(e, m , d){
 				 
 				}
 		})
-		.attr("d", pathG)
+		.attr("d", pathG) //finally call the path funciton to draw the paths
 	
+	
+	//add text for labels and position at the shape centroid.
 	group.selectAll("text").data(map.features).enter().append("text")
 		.text(
 			function(d){
